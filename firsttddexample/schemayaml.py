b4f9504434ea9d1b9e1899e816438a0b0b5f7bf0
Adrian Farnell <adrianjfarnell@gmail.com>
2021-01-03 22:07:17 +0000

    ppeppered with comments

--- firsttddexample/schemayaml.py
+++ firsttddexample/schemayaml.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 
+#Import all dependencies
 from cerberus import Validator
 import yaml
 import json
 
+#bit of formatting
 print()
 
-with open('IsDockerComposeAtVersion3.json', 'r') as file:
+#I want an iterative loop to build a list of json files to read through
+with open('test_IsDockerComposeAtVersion3.json', 'r') as file:
     schema = file.read()
 
+#setup a cerberus validator
 v = Validator()
+#convert json schema to a python dict (cerberus req)
 v.schema = json.loads(schema)
 
+#some debug info. 
 print(" *DEBUG* Schema: " + schema)
 
+#open the file we want to test. 
 with open('docker-compose.yml') as f:
 
+    #load up an dict that contains all of our yaml data
     data = yaml.load(f, Loader=yaml.FullLoader)
-    print(" *DEBUG* Yaml: " + data['version'])
+    
+    #bit more debug info, shows actual data in the part we're testing
+    print(" *DEBUG* Inside the Yaml: " + data['version'])
 
+    #and I want to some how functionalise this to iteratively test my yml file. 
+    #TODO check the logic here, not convinced its actually testing what I want it to
     if v.validate({'version': data['version']}):
         print(' TEST PASSED')
     else:
